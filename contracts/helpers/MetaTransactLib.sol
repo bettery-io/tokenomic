25fd8e5f43527d452648620b3ad7fb3a72eb70d8
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-02-12 13:43:16 +0200

    added common folder to the project

--- contracts/helpers/MetaTransactLib.sol
+++ contracts/helpers/MetaTransactLib.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MetaTransactLib {
+contract MetaTransactLib is AccessControl {
     
     function _msgSender()
         internal
+        override
         view
         returns (address payable sender)
     {
