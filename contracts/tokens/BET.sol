cd3e4aaa1c45bcf5cf6924ab904b2a84ebfef902
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-02-12 13:30:57 +0200

    init

---
+++ contracts/tokens/BET.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BET is ERC20 {
+        constructor(
+         string memory _name,
+         string memory _symbol,
+         uint8 _decimals,
+         uint256 _initialSupplyCoins
+        ) public ERC20(_name, _symbol) {
+
+            _setupDecimals(_decimals);
+            _mint(msg.sender, _initialSupplyCoins * (10**uint256(_decimals)));
+        }
+
+}
